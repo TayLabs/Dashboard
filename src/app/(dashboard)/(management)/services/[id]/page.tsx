b7cb1df28f9b06616789967f8da74f0c637d528d
Taylor Kelley <taylorgkelley@outlook.com>
2026-01-06 21:19:58 -0600

    fix: a few page logic changes for id's and adding get services

--- src/app/(dashboard)/(management)/services/[id]/page.tsx
+++ src/app/(dashboard)/(management)/services/[id]/page.tsx
@@ -1,13 +1,34 @@
 import { UUID } from 'node:crypto';
 
 type EditServicePageProps = {
-  params: Promise<{ id: 'add' | `${UUID}_${UUID}` }>;
+	params: Promise<{ id: 'add' | `${UUID}_${UUID}` }>;
 };
 
-export default async function EditServicePage({
-  params,
-}: EditServicePageProps) {
-  const { id } = await params;
+const uuidRegex =
+	/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
-  return <div>EditServicePage: {id}</div>;
+export default async function EditServicePage({
+	params,
+}: EditServicePageProps) {
+	const { id, isAdd } = await params.then(
+		({ id }): { id?: UUID[]; isAdd?: boolean } => {
+			if (id === 'add') {
+				return { isAdd };
+			} else if (uuidRegex.test(id)) {
+				return { id: [id] };
+			} else if (id.split('_').every((uuid) => uuidRegex.test(uuid))) {
+				const [first, second] = id.split('_');
+
+				return {
+					id: [first as UUID, second as UUID],
+				};
+			}
+
+			return {};
+		}
+	);
+
+  const response = await getService(id)
+
+	return <div>EditServicePage: {id}</div>;
 }
