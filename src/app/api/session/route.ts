402cc78ab42e70bf7b62051094d06bbae8c7a757
Taylor Kelley <taylorgkelley@outlook.com>
2026-01-11 17:56:17 -0600

    chore: adding comment to explain route

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,13 +1,17 @@
 import { cookies } from 'next/headers';
 
+/**
+ * A endpoint for the client to get the access token to make requests
+ * @returns Access token cookie's value
+ */
 export async function GET() {
-  try {
-    const cookieStore = await cookies();
+	try {
+		const cookieStore = await cookies();
 
-    const accessToken = cookieStore.get('_access_t');
+		const accessToken = cookieStore.get('_access_t');
 
-    return Response.json({ success: true, data: { accessToken } });
-  } catch (error) {
-    return Response.json({ success: false, error: (error as Error).message });
-  }
+		return Response.json({ success: true, data: { accessToken } });
+	} catch (error) {
+		return Response.json({ success: false, error: (error as Error).message });
+	}
 }
