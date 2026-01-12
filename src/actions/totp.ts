a6203d83c9f5688ca6f8d8d769a46b6b887f6c24
Taylor Kelley <taylorgkelley04@gmail.com>
2026-01-12 11:40:50 -0600

    fix: adding cookie setting to set new token's returned without pending 2fa

--- src/actions/totp.ts
+++ src/actions/totp.ts
@@ -4,6 +4,7 @@ import { getAccessToken } from '@/lib/auth';
 import { FormActionResponse } from './types/FormAction';
 import { getCSRFToken } from '@/lib/auth/csrf';
 import { cookies } from 'next/headers';
+import { parseCookie } from '@/utils/cookies';
 
 export async function verifyTOTP({
   code,
@@ -37,9 +38,33 @@ export async function verifyTOTP({
       }
     );
 
-    return {
-      success: true,
-    };
+    const resBody = await response.json();
+
+    if (!resBody?.success) {
+      throw new Error(resBody.message);
+    } else {
+      const token = resBody.data.accessToken;
+      cookieStore.set('_access_t', token, {
+        // expires: ... // Session cookie, refreshes on each new visit/session
+        httpOnly: true,
+        path: '/',
+        domain: 'localhost',
+        sameSite: 'lax',
+      });
+
+      // Set cookies for login action
+      const cookieHeaders = response.headers.getSetCookie();
+
+      for (const cookieHeader of cookieHeaders) {
+        const cookie = parseCookie(cookieHeader);
+
+        cookieStore.set(cookie);
+      }
+
+      return {
+        success: true,
+      };
+    }
   } catch (error) {
     return {
       success: false,
