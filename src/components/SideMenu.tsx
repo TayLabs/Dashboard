7ea1d3f82671d0a6a6d45b731369d14a2ef72b82
TaylorGKelley <taylorgkelley04@gmail.com>
2026-03-26 09:27:10 -0500

    fix: bug with alert element's from home page showing an error showing above the nav menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { useIsMobile } from '@/hooks/use-mobile';
-import { useSideMenu } from '@/hooks/use-sidemenu';
-import { cn } from '@/utils';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { useIsMobile } from "@/hooks/use-mobile";
+import { useSideMenu } from "@/hooks/use-sidemenu";
+import { cn } from "@/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type MenuItem = {
   title: string;
@@ -12,20 +12,20 @@ type MenuItem = {
 };
 
 const menu = {
-  '/': [
-    { title: 'Home', url: '/' },
-    { title: 'Services', url: '/services' },
-    { title: 'Keys', url: '/keys' },
-    { title: 'Roles', url: '/roles' },
-    { title: 'Users', url: '/users' },
+  "/": [
+    { title: "Home", url: "/" },
+    { title: "Services", url: "/services" },
+    { title: "Keys", url: "/keys" },
+    { title: "Roles", url: "/roles" },
+    { title: "Users", url: "/users" },
   ],
-  '/account': [
-    { title: 'Profile', url: '/account' },
-    { title: 'Security', url: '/account/security' },
+  "/account": [
+    { title: "Profile", url: "/account" },
+    { title: "Security", url: "/account/security" },
   ],
 } as Record<string, MenuItem[]>;
 
-const sideMenuWidthClassName = 'md:w-72';
+const sideMenuWidthClassName = "md:w-72";
 
 export default function SideMenu() {
   const pathname = usePathname();
@@ -33,32 +33,34 @@ export default function SideMenu() {
     menu[
       Object.keys(menu)
         .filter((key) => pathname.startsWith(key))
-        .reduce((acc, curr) => (acc.length > curr.length ? acc : curr), '') // Find the most specific matching menu item (longest one is most specific)
+        .reduce((acc, curr) => (acc.length > curr.length ? acc : curr), "") // Find the most specific matching menu item (longest one is most specific)
     ];
 
   const { isOpen } = useSideMenu();
   const isMobile = useIsMobile();
 
   return (
-    <div className={cn('h-full', sideMenuWidthClassName)}>
+    <div className={cn("h-full", sideMenuWidthClassName)}>
       <aside
         className={cn(
-          'fixed bg-background transition-opacity duration-300',
+          "fixed bg-background transition-opacity duration-300 z-50",
           sideMenuWidthClassName,
           {
-            'top-18 bottom-0 left-0': !isMobile,
-            'top-18 inset-x-0 bottom-0': isMobile,
-            'opacity-0 pointer-events-none': isMobile && !isOpen,
-            'opacity-100 pointer-events-auto': isMobile && isOpen,
-          }
-        )}>
+            "top-18 bottom-0 left-0": !isMobile,
+            "top-18 inset-x-0 bottom-0": isMobile,
+            "opacity-0 pointer-events-none": isMobile && !isOpen,
+            "opacity-100 pointer-events-auto": isMobile && isOpen,
+          },
+        )}
+      >
         {/* main side menu items */}
         <nav className="list-none p-4 flex flex-col gap-2 h-full overflow-y-auto">
           {menuItems.map(({ title, url }, i) => (
             <Link
               key={`${i}-${title}`}
               className="text-xl font-medium hover:bg-muted hover:shadow-sm cursor-pointer transition-[background-color_box-shadow] duration-300 px-5 py-3 rounded-lg"
-              href={url}>
+              href={url}
+            >
               {title}
             </Link>
           ))}
