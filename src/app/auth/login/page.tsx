1c24606ce9a55e89a02feb019c87ca28c3527f30
TaylorGKelley <taylorgkelley04@gmail.com>
2026-03-26 09:31:34 -0500

    fix: adding suspense around login form so it loads with new useSearchParams hook

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,22 @@
-import LoginForm from './LoginForm';
+import { Suspense } from "react";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-	return (
-		<section className='container max-w-sm'>
-			<h1 className='text-3xl font-semibold mb-4'>Welcome back</h1>
-			<p className='text-muted-foreground mb-10'>
-				Login to manage your environment.
-			</p>
-			<LoginForm />
-		</section>
-	);
+  return (
+    <section className="container max-w-sm">
+      <h1 className="text-3xl font-semibold mb-4">Welcome back</h1>
+      <p className="text-muted-foreground mb-10">
+        Login to manage your environment.
+      </p>
+      <Suspense
+        fallback={
+          <div className="text-muted-foreground px-4 py-3 w-full text-center">
+            Loading...
+          </div>
+        }
+      >
+        <LoginForm />
+      </Suspense>
+    </section>
+  );
 }
